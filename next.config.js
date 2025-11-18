dca48460fbb5ab24bb21c81bb021542dd3eb408c
Niltinho77 <nilton.cn1992@aluno.ifsc.edu.br>
2025-11-17 21:37:49 -0300

    retirando o problema

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-    assetPrefix: isProd ? '/NextJS-Tailwind-App-Presentation-Page/' : '',
-    basePath: isProd ? '/NextJS-Tailwind-App-Presentation-Page' : '',
-    output: 'export',
-    images: {
-      unoptimized: true, // Disable default image optimization
-    },
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // opcional
+  },
 };
 
 module.exports = nextConfig;
